98cc2c697951e5d4c01b97e1402833f87077ff08
David luque Alba <dvdlq99@gmail.com>
2020-12-16 15:48:26 +0100

    connected to backend

--- src/services/ExerciseService.js
+++ src/services/ExerciseService.js
@@ -6,9 +6,9 @@ class ExerciseService {
 
   constructor() {
     let service = axios.create({
-      baseURL: "http://localhost:3000",
-      // "https://dvdlq99-react-project.herokuapp.com",
+      baseURL: "https://dvdlq99-react-project.herokuapp.com",
       
+      // "http://localhost:3000",
     });
 
 
