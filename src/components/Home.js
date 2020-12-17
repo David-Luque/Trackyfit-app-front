877ad2191dd92e419cb4da28678b1d563a968ed6
David luque Alba <dvdlq99@gmail.com>
2020-12-17 09:19:28 +0100

    home visuals

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,24 +5,29 @@ import '../styles/Home.css'
 const Home = ()=>{
   return(
     <div className="Home">
-      <aside>
-        Would you like to track your performance progress and health condition?
-      </aside>
-      <hr />
-      <article>
-        <h3>Keep your physical condition under control</h3>
-        <p>
-          You can record your weight and also your metrics of the main muscle groups to clearly track your physical progress
-        </p>
+      <header className="header">
+        TRACK YOUR PERFORMANCE PROGRESS AND HEALTH CONDITION
+      </header>
+      
+      <article className="article-metrics">
+        <div className="article-text">
+          <h3>Keep your physical metrics under control</h3>
+          <hr />
+          <p>
+            You can record your weight and also your metrics of the main muscle groups to clearly track your physical progress
+          </p>
+        </div>
       </article>
-      <hr />
-      <article>
-        <h3>Encourage yourself to improve in every workout</h3>
-        <p>
-          Save your training results to check your performance improvement
-        </p>
+      
+      <article className="article-workout">
+        <div className="article-text">
+          <h3>Encourage yourself to improve in every workout</h3>
+          <hr />
+          <p>
+            Save your training results to check your performance improvement
+          </p>
+        </div>
       </article>
-      <hr />
     </div>
   )
 }
